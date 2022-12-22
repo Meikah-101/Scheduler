d6dd963081a451a01360d26f5894aa2f6deb68c8
Meikah Robinson <meikah@localhost.localdomain>
2022-12-22 18:07:18 -0500

    forgot to save changes to file in first commit

--- sch_cvs_writer.py
+++ sch_cvs_writer.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np 
 import random
 import csv
-old_schedule = input("Enter name of original csv file :")
-    
+
+old_schedule = input("Enter name of original csv file :")    
 names_txtfile = input("Enter name of original txt file with names inside") 
 
 file = pd.read_csv(old_schedule)
@@ -18,32 +18,40 @@ names_column_nowhite = replace_nan.strip()
 names_column_split = names_column_nowhite.split(",")
 
 
-with open(names_txtfile) as f: 
+with open("names.txt") as f: 
     namesfile = f.read()
 names_nowhite = namesfile.replace(" ","" )
+names_nowhite = names_nowhite.strip()
 names_not_found = names_nowhite.split(",")
-names_not_found = str(names_not_found)
-names_not_found = names_not_found.strip()
-new_names_set = set(names_not_found).difference(names_column_split) # finds the names that are not found in the original schedule
+new_names_set = set(names_not_found).difference(names_column_split)  # finds the names that are not found in the original schedule
 new_names_list = list(new_names_set)
 new_names_random = random.sample(new_names_list, len(new_names_list))
-new_names_random = new_names_random
 name_counter = 0
 
+with open('example_old_sch.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        if len(names_not_found) > len(names_column):
+            print("some names might not have been assigned to a date due to more names then avaiable assignable spots present")
+            break
 
 for index, row in file.iterrows():
-    if pd.isna(row.iloc[1]):
-        date = row.iloc[0]
-        name = new_names_random[name_counter]
-        name_counter = name_counter +1 # iterates through new_names_random 
-        
-        data = {'dates': [date], 'names': [name]}
-        new_entry = pd.DataFrame(data=data) #creates new pandas dataframe
-        new_schedule = new_schedule.append(new_entry)
-        
+        if pd.isna(row.iloc[1]):
+            if name_counter < len(new_names_random):
+                date = row.iloc[0]
+                name = new_names_random[name_counter]
+                name_counter = name_counter +1 # iterates through new_names_random 
+                data = {'dates': [date], 'names': [name]}
+                new_entry = pd.DataFrame(data=data) #creates new pandas dataframe
+                new_schedule = new_schedule.append(new_entry)
 
-    else:
-        new_schedule = new_schedule.append(row) #adds data from orginal csv schedule if nan values are absent 
+        else:
+             new_schedule = new_schedule.append(row) #adds data from orginal csv schedule if nan values are absent 
         
-
 new_schedule.to_csv('new_sch.csv' , index = False , columns = ['dates', 'names']) #creates new csv with the new data 
+
+with open('example_old_sch.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile)
+    for row in csvreader:
+        if not row[1]:
+             print("these dates possibly will have no name assigned due to not being enough names present to pair with dates:" + row[0])
